f6fa0bf2c8bfb64fe94f221f222507743741d42d
Admiral-Simo <admiralsimo@gmail.com>
2023-02-02 10:58:59 +0100

    Feature Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+
 module.exports = {
   content: [
     "./public/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
+    },
   },
   plugins: [],
 }
